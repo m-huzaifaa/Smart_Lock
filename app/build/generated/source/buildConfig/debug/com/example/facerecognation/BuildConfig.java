e8e7eaa39e2ed9eef19e97967d1ea9611621e911
bushra2001 <bushra_ashfaq8@yahoo.com>
2023-02-04 05:32:12 -0600

    Adding file_path.xml

---
+++ app/build/generated/source/buildConfig/debug/com/example/facerecognation/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.example.facerecognation;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.example.facerecognation";
+  public static final String BUILD_TYPE = "debug";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+}
